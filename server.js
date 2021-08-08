d005ff3bf25b3479d3eb0009d5b54622f2b3e884
software12@varaluae.com <keerthanakumar2013@gmail.com>
2021-08-08 12:48:20 +0530

    Changed

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world demo1\n');
+  res.send('Hello world demo purpose\n');
 });
 
 app.listen(PORT);
